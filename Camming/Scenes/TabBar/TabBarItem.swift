15e19cce60b48135d8022536f41263ef2bbf0b7a
victorrr91 <winhunu3@naver.com>
2023-01-08 19:03:28 +0900

    주석 정리

--- Camming/Scenes/TabBar/TabBarItem.swift
+++ Camming/Scenes/TabBar/TabBarItem.swift
@@ -9,25 +9,16 @@ import UIKit
 
 enum TabBarItem: CaseIterable {
     case checklist
-//    case calendar
-//    case search
-//    case etc
 
     var icon: (default: UIImage?, selected: UIImage?) {
         switch self {
         case .checklist: return (UIImage(systemName: "checklist"), UIImage(systemName: "checklist.fill"))
-//        case .calendar: return (UIImage(systemName: "calendar"), UIImage(systemName: "calendar.fill"))
-//        case .search: return (UIImage(systemName: "magnifyingglass"), UIImage(systemName: "magnifyingglass.fill"))
-//        case .etc: return (UIImage(systemName: "ellipsis"), UIImage(systemName: "ellipsis.fill"))
         }
     }
 
     var viewController: UIViewController {
         switch self {
         case .checklist: return UINavigationController(rootViewController: ChecklistViewController())
-//        case .calendar: return UIViewController()
-//        case .search: return UIViewController()
-//        case .etc: return UIViewController()
         }
     }
 }
